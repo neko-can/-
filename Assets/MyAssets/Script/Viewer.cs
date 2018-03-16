430433e455ea77743839f1c5cd9d0f7a2c2c861a
neko-can <34763150+neko-can@users.noreply.github.com>
2018-03-17 06:36:38 +0900

    restart

---
+++ Assets/MyAssets/Script/Viewer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viewer : MonoBehaviour
+{
+
+    /// <summary>
+    /// 機能
+    /// ・常に視界の中央にObjectを捉え、頭の回転でObjectを全方向から眺められるようにする。
+    /// ・外部のObjectに付与
+    /// →Main CameraのCenterOfMassを変更すればいい。→効果なし
+    /// →カメラのforwardを基に計算
+    /// </summary>
+
+    public GameObject VRCamera;
+    public GameObject targetObject;
+
+    public float distance;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VRCamera.transform.position = targetObject.transform.position - distance * VRCamera.transform.forward;
+    }
+}
---
+++ Assets/MyAssets/Script/unitychan_CNTRL.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unitychan_CNTRL : MonoBehaviour {
+
+    public GameObject unitychan;
+
+    float baseSpeed = 6f;
+
+    Animator unitychan_Anim;
+
+	// Use this for initialization
+	void Start () {
+        unitychan_Anim = unitychan.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void LateUpdate()
+    {
+        unitychan_Anim.SetFloat("Speed", baseSpeed);
+
+    }
+}
