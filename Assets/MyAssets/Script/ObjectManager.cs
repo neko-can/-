4fe8a1f7595822ae6e23202f8840698c450824f4
neko-can <shi8teikoh@gmail.com>
2018-03-18 07:18:57 +0900

    add boundsTest

--- Assets/MyAssets/Script/ObjectManager.cs
+++ Assets/MyAssets/Script/ObjectManager.cs
@@ -6,23 +6,27 @@ using UnityEngine.UI;
 public class ObjectManager : MonoBehaviour {
 
     public GameObject WallsParent;
+    public GameObject floor;
 
     public List<GameObject> buttonList;
     public List<GameObject> wallList;
     int noObjects;
-
     GameObject cloneTarget;
 
-    int i;
+    Mesh floorMesh;
+    Renderer floorRenderer;
 
 	// Use this for initialization
 	void Start () {
 
         noObjects = wallList.Count;
-        for (i=0; i<noObjects; i++)
+        for (int i=0; i<noObjects; i++)
         {
-            buttonList[i].GetComponent<Button>().onClick.AddListener(OnClick);
+            int ii = i + 0; //一時変数
+            buttonList[i].GetComponent<Button>().onClick.AddListener(() => OnClick(ii));
         }
+
+        makeSelectableZone();
 	}
 	
 	// Update is called once per frame
@@ -30,10 +34,25 @@ public class ObjectManager : MonoBehaviour {
 		
 	}
 
-    public void OnClick()
+    public void OnClick(int i)
     {
-        Debug.Log(i);
         cloneTarget = GameObject.Instantiate(wallList[i], WallsParent.transform);
     }
 
+    void makeSelectableZone()
+    {
+        //floorのサイズは正規化されているものとする。
+        floorRenderer = floor.GetComponent<Renderer>();
+
+    }
+
+    void testBounds()
+    {
+        floorMesh = floor.GetComponent<MeshFilter>().mesh;
+
+        Debug.Log("vertices : " + floorMesh.vertices[0].ToString());
+        Debug.Log("bounds.size : " + floorMesh.bounds.size.ToString());
+        Debug.Log("renderer's bounds : " + floorRenderer.bounds.size.ToString());
+        Debug.Log("renderer's center : " + floorRenderer.bounds.center.ToString());
+    }
 }
