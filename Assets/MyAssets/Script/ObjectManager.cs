f111c32c9f95adfe2fe763e5a1fcb58df246aa19
neko-can <shi8teikoh@gmail.com>
2018-03-18 11:47:35 +0900

    add move

--- Assets/MyAssets/Script/ObjectManager.cs
+++ Assets/MyAssets/Script/ObjectManager.cs
@@ -8,27 +8,42 @@ public class ObjectManager : MonoBehaviour {
     public GameObject WallsParent;
     public GameObject floor;
     public GameObject selectableZone;
+    public GameObject selectableZoneParent;
+    public GameObject targetController; //ボタンの親
 
+    //UIで補助
     public List<GameObject> buttonList;
     public List<GameObject> wallList;
+    public GameObject editerPanel;
     int noObjects;
     GameObject cloneTarget;
 
+    //オブジェクトで補助
     Mesh floorMesh;
     Renderer floorRenderer;
     Vector3 startPos;
+    List<Vector3> selectableZonesPos = new List<Vector3>();
     Vector3 floorBoundsSize;
     Vector3 floorCenter;
+    GameObject selectableZoneClone;
+    Vector2 noDivision;
+    int noZone;
+
+    //ターゲットコントローラー
+    GameObject upButton;
+    GameObject leftButton;
+    GameObject rightButton;
+    GameObject downButton;
+    GameObject targetObject;
+    int posIndex = 0;
 
 	// Use this for initialization
 	void Start () {
 
-        noObjects = wallList.Count;
-        for (int i=0; i<noObjects; i++)
-        {
-            int ii = i + 0; //一時変数
-            buttonList[i].GetComponent<Button>().onClick.AddListener(() => OnClick(ii));
-        }
+        setSelectableZonesPos();
+        setListButton();
+        makeSelectableZone();
+        setControllerButton();
 
 	}
 	
@@ -37,20 +52,88 @@ public class ObjectManager : MonoBehaviour {
 		
 	}
 
+    void setControllerButton()
+    {
+        upButton = targetController.transform.Find("upButton").gameObject;
+        leftButton = targetController.transform.Find("leftButton").gameObject;
+        rightButton = targetController.transform.Find("rightButton").gameObject;
+        downButton = targetController.transform.Find("downButton").gameObject;
+
+        rightButton.GetComponent<Button>().onClick.AddListener(rightOnClick);
+        leftButton.GetComponent<Button>().onClick.AddListener(leftOnClick);
+        upButton.GetComponent<Button>().onClick.AddListener(upOnClick);
+        downButton.GetComponent<Button>().onClick.AddListener(downOnClick);
+        
+    }
+
+    void leftOnClick()
+    {
+        posIndex = (noZone + posIndex - 1) % noZone;
+        targetObject.transform.position = selectableZonesPos[posIndex];
+    }
+
+    void rightOnClick()
+    {
+        posIndex = (posIndex + 1) % noZone;
+        targetObject.transform.position = selectableZonesPos[posIndex];
+    }
+
+    void upOnClick()
+    {
+        posIndex = (noZone + posIndex - (int)noDivision.x) % noZone;
+        targetObject.transform.position = selectableZonesPos[posIndex];
+    }
+
+    void downOnClick()
+    {
+        posIndex = (posIndex + (int)noDivision.x) % noZone;
+        targetObject.transform.position = selectableZonesPos[posIndex];
+    }
+
+    void setListButton()
+    {
+        noObjects = wallList.Count;
+        for (int i = 0; i < noObjects; i++)
+        {
+            int ii = i + 0; //一時変数
+            buttonList[i].GetComponent<Button>().onClick.AddListener(() => OnClick(ii));
+        }
+    }
+
     public void OnClick(int i)
     {
         cloneTarget = GameObject.Instantiate(wallList[i], WallsParent.transform);
+        cloneTarget.transform.position = selectableZonesPos[posIndex];
+
+        targetObject = cloneTarget;
+        posIndex = 0;
+        editerPanel.GetComponent<Animator>().SetTrigger("selected");
     }
 
-    void makeSelectableZone()
+    void setSelectableZonesPos()
     {
-        //floorのサイズは正規化されているものとする。
         floorRenderer = floor.GetComponent<Renderer>();
         floorBoundsSize = floorRenderer.bounds.size;
         floorCenter = floorRenderer.bounds.center;
 
-        startPos = new Vector3(-floorBoundsSize.x / 2, 0, -floorBoundsSize.z / 2);
+        startPos = new Vector3(-floorBoundsSize.x / 2 + 0.5f, 0, -floorBoundsSize.z / 2 + 0.5f);
 
+        noDivision = new Vector2(floorBoundsSize.x, floorBoundsSize.z);
+
+        noZone = (int)(noDivision.x * noDivision.y);
+        for (int i = 0; i < noZone; i++)
+        {
+            selectableZonesPos.Add(new Vector3(startPos.x + i / (int)noDivision.x, 0, startPos.z + i % (int)noDivision.y));
+        }
+    }
+
+    void makeSelectableZone()
+    {
+        for(int i=0; i<noZone; i++)
+        {
+            selectableZoneClone = GameObject.Instantiate(selectableZone, selectableZoneParent.transform);
+            selectableZoneClone.transform.position = selectableZonesPos[i];
+        }
     }
 
     void testBounds()
