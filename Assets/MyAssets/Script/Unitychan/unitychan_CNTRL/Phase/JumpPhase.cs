07ca2924764fab6c24596eb00e9b70d1b0168a54
neko-can <34763150+neko-can@users.noreply.github.com>
2018-03-31 22:56:05 +0900

    add ray

--- Assets/MyAssets/Script/Unitychan/unitychan_CNTRL/Phase/JumpPhase.cs
+++ Assets/MyAssets/Script/Unitychan/unitychan_CNTRL/Phase/JumpPhase.cs
@@ -15,12 +15,20 @@ public class JumpPhase : MonoBehaviour {
     bool IsOnJump = true;
     UnitychanCollider unitychanCollider;
     float unitychanAnimTime;
+    Ray ray;
+    RaycastHit raycastHit;
+    Vector3 rayStartPos;
+    Vector3 rayDirection;
+    Vector3 rayPos;
+    float jumpTime;
+    public GameObject rayObject;
+    GameObject rayClone;
     //parameter
-    float firstVelocityY = 15f;
+    float firstVelocityY = 11f;
     float firstVelocityF = 5f;
-    float maxHeight = 6f;
-    float maxHeightTime = 0.5f;
+    //float maxHeightTime = 0.5f;
     float jumpStartTime = 0.2f;
+    float rayStopTime = 0.4f;
 
     public void MyStart()
     {
@@ -28,7 +36,6 @@ public class JumpPhase : MonoBehaviour {
         unitychan_Anim = Unitychan_CNTRL.unitychan_Anim;
         unitychan = Unitychan_CNTRL.unitychan;
         unitychanRb = unitychan.GetComponent<Rigidbody>();
-        upSpeed = maxHeight / maxHeightTime;
         unitychanCollider = Unitychan_CNTRL.unitychanCollider;
     }
 
@@ -40,20 +47,18 @@ public class JumpPhase : MonoBehaviour {
         //初速付与
         if (IsOnJump && unitychanAnimTime >jumpStartTime)
         {
-            unitychanVelocity = unitychanRb.velocity;
-            unitychanVelocity += unitychan.transform.forward * firstVelocityF + new Vector3(0, firstVelocityY);
             unitychanRb.velocity = unitychanVelocity;
 
             //unitychanRb.AddForce(new Vector3(0, 100, 0));
             IsOnJump = false;
         }
         //落下速度上昇
-        if (unitychanAnimTime > maxHeightTime)
-        {
-            //unitychanVelocity = unitychanRb.velocity;
-            //unitychanVelocity.y -= 1.3f;
-            //unitychanRb.velocity = unitychanVelocity;
-        }
+        //if (unitychanAnimTime > maxHeightTime)
+        //{
+        //    unitychanVelocity = unitychanRb.velocity;
+        //    unitychanVelocity.y -= 1.3f;
+        //    unitychanRb.velocity = unitychanVelocity;
+        //}
         //壁キック遷移判定
         //if (unitychanCollider.IsHit)
         //{
@@ -67,11 +72,29 @@ public class JumpPhase : MonoBehaviour {
     void JudgeToSecondJump()
     {
         //firstVelocity設定後にRayを飛ばし、当たったら遷移
+        rayStartPos = unitychan.transform.position;
+        rayDirection = unitychan.transform.forward;
+        ray = new Ray(rayStartPos, rayDirection);
     }
 
     public void OnChanged()
     {
+        //初速度変更
+        //初速度取得
+        unitychanVelocity = unitychanRb.velocity;
+        unitychanVelocity += unitychan.transform.forward * firstVelocityF + new Vector3(0, firstVelocityY);
 
+        //落下時間計算
+        jumpTime = Mathf.Abs(2 * unitychanVelocity.y / Physics.gravity.y);
+        Debug.Log(jumpTime);
+        //Ray情報取得
+        rayPos = unitychan.transform.position;
+        rayPos.x += unitychanVelocity.x * rayStopTime * jumpTime;
+        rayPos.z += unitychanVelocity.z * rayStopTime * jumpTime;
+        rayPos.y += unitychanVelocity.y * rayStopTime * jumpTime + 0.5f * Physics.gravity.y * Mathf.Pow(rayStopTime * jumpTime, 2);
+        //ray視覚化
+        rayClone = GameObject.Instantiate(rayObject);
+        rayClone.transform.position = rayPos;
     }
 
     public void OnEnd()
